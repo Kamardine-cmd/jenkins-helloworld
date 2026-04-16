2c8b4f82f30f18a6f6af36b9849e3cfb40b598bd
Kamardine-cmd <abdallahkamardineoili@gmail.com>
2026-04-16 10:54:40 +0200

    rajout-1

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Kamardine, tu es au courant qu'on est chanceux d'être en bonne santé? 
-        Que des bonnes nouvelles pour nous avec beaucoup d'espoir ");
+        Que des bonnes nouvelles pour nous avec beaucoup d'espoir ". avec tout le monde)
     }
 }
 
