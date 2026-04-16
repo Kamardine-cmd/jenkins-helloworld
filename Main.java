72ddbea0686cf0e638f8d1b89b109f3728e81b44
Kamardine-cmd <abdallahkamardineoili@gmail.com>
2026-04-16 11:13:39 +0200

    ça fonctionne

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Kamardine, tu es au courant qu'on est chanceux d'être en bonne santé?")
+        System.out.println("Hello Kamardine, tout fonctionne ! ");
         
     }
 }
